7ca81cde3a31bb251f8660fac0f6b4dacba7cfbb
june29 <june29.jp@gmail.com>
2012-03-14 00:56:09 +0900

    rails new kceffect --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kceffect::Application.config.secret_token = '07002e58581a15640bbefe2c116bf375db2557a2049fd108527b5e1e72e9f771acda1dd92c9923b5749888d6e51563042e299c7e4f37c20c998fc3e3a13d8f0a'
