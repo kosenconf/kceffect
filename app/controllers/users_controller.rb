da27ab503298f882cfdf05bafb7475b7129b085f
june29 <june29.jp@gmail.com>
2012-05-27 20:12:49 +0900

    adjust views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_name(params[:id])
+    @user    = User.find_by_name(params[:id])
+    @effects = @user.effects.order("effected_at DESC")
   end
 end
