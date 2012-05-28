a27f5f6689f7e2a208a0e6086b27df7417dde50e
june29 <june29.jp@gmail.com>
2012-05-29 02:41:48 +0900

    add cover and stats to top page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     @effects = Effect.order("id DESC")
     @events  = Event.order("start_at DESC")
     @tags    = Tag.where(:category => nil).order("id DESC")
+
+    @covers  = Event.where("cover_url IS NOT NULL").reject { |e| e.cover_url.blank? }.map(&:cover_url).shuffle
   end
 
   def about
