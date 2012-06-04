9b982c55442a31ea1a68d53e6b294ced9be02dae
june29 <june29.jp@gmail.com>
2012-06-05 02:11:13 +0900

    destroy related tag when last one tagging is destroyed

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -8,4 +8,9 @@ class Tagging < ActiveRecord::Base
   belongs_to :tag
   belongs_to :effect
   belongs_to :user
+
+  after_destroy { |record|
+    tag = record.tag
+    tag.destroy if !tag.event? && tag.taggings.count == 0
+  }
 end
