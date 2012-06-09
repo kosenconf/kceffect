8b3fa082d36461f295e18d5db7ed6a30db5ff663
hmsk <k.hamasaki@gmail.com>
2012-06-10 02:24:48 +0900

    slimmed cover

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
     @users   = User.order("updated_at DESC").limit(100)
     @tags    = Tag.where(:category => nil).order("id DESC")
 
-    @covers  = Event.where("cover_url IS NOT NULL").reject { |e| e.cover_url.blank? }.take(9).map(&:cover_url).shuffle
+    @covers  = Event.where("cover_url IS NOT NULL").reject { |e| e.cover_url.blank? }.take(6).map(&:cover_url).shuffle
 
     @social_footer = true
   end
