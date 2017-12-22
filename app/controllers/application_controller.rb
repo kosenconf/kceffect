eb99348a1e0a0a0f588b670d6341e1b151e16afe
asonas <hzw1258@gmail.com>
2017-12-22 16:43:35 +0900

    Follow up rails 4.2.x

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
   before_filter :please_sign_in_again
-
-  protect_from_forgery
-
   helper_method :current_user, :signed_in?, :sign_out!
 
   protected
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Kceffect::Application.config.session_store :cookie_store, key: '_kceffect_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Kceffect::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_kceffect_session'
